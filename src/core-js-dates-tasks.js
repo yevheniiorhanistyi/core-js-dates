2963c0d60dbe7e29da66438a0db900fdb1606f96
Yevhenii Orhanistyi <organistiy@gmail.com>
2025-03-01 15:38:59 +0100

    feat: implement formatDate function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -138,8 +138,17 @@ function isDateInPeriod(date, period) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  return new Date(date).toLocaleString('en-US', {
+    timeZone: 'UTC',
+    month: 'numeric',
+    day: 'numeric',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true,
+  });
 }
 
 /**
