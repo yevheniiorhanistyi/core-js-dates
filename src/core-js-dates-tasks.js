9403e47c4e41c642b6ea3eee73670e06e3462034
Yevhenii Orhanistyi <organistiy@gmail.com>
2025-03-02 11:07:53 +0100

    feat: implement getNextFridayThe13th function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -212,8 +212,14 @@ function getWeekNumberByDate(date) {
  * Date(2024, 0, 13) => Date(2024, 8, 13)
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
-function getNextFridayThe13th(/* date */) {
-  throw new Error('Not implemented');
+function getNextFridayThe13th(date) {
+  const nextDate = new Date(date);
+
+  do {
+    nextDate.setDate(nextDate.getDate() + 1);
+  } while (nextDate.getDay() !== 5 || nextDate.getDate() !== 13);
+
+  return nextDate;
 }
 
 /**
